b2b7f1c9c0af957c39f284b6830da5ff8352dc30
Shatei-a11 <80773384+Shatei-a11@users.noreply.github.com>
2021-03-18 19:58:16 -0300

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import os
-os.chdir('/data/data/com.termux/files/home/termux_senha')
+os.chdir('/data/data/com.termux/files/home/senha-termux')
 os.system('cp caminho /data/data/com.termux/files/usr/etc/')
 os.chdir('/data/data/com.termux/files/usr/etc/')
 os.system('rm -rf bash.bashrc')
